9600d18e94bcfe15b4a4eeb490633639a5ee64e2
prisis <d.bannert@anolilab.de>
2021-01-22 20:44:33 +0100

    feat: added release config

---
+++ release.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+    "dryRun": true,
+    "extends": "semantic-release-monorepo",
+    "plugins": [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "conventionalcommits",
+            },
+        ],
+        [
+            "@google/semantic-release-replace-plugin",
+            {
+                "replacements": [
+                    // {
+                    //     "files": [
+                    //         "README.md",
+                    //     ],
+                    //     "from": "@v.*/mod.ts",
+                    //     "to": "@v${nextRelease.version}/mod.ts",
+                    //     "results": [
+                    //         {
+                    //             "file": "README.md",
+                    //             "hasChanged": true,
+                    //             "numMatches": 1,
+                    //             "numReplacements": 1,
+                    //         },
+                    //     ],
+                    //     "countMatches": true,
+                    // },
+                ],
+            },
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                "preset": "conventionalcommits",
+            },
+        ],
+        "@semantic-release/changelog",
+        "@semantic-release/github",
+        [
+            "@semantic-release/git",
+            {
+                "assets": [
+                    "package.json",
+                    "dist/*",
+                    "README.md",
+                    "UPGRADE.md",
+                    "LICENSE.md.md",
+                    "CHANGELOG.md",
+                ],
+            },
+        ],
+    ],
+};
